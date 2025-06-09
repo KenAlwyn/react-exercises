6fcb4f044d088e0a9c13f8fe4da70fa55f0b38cd
KenAlwyn <ksvalenciano1@up.edu.ph>
2025-06-09 11:25:46 +0800

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import "./App.css";
+
+
+function App() {
+  const [task, setTask] = useState("");
+  const [tasks, setTasks] = useState([]);
+
+  const handleAdd = () => {
+    if (task.trim() === "") return;
+    setTasks([...tasks, task]);
+    setTask("");
+  };
+
+  const handleDelete = (index) => {
+    const newTasks = tasks.filter((_, i) => i !== index);
+    setTasks(newTasks);
+  };
+
+  const handleDeleteAll = () => {
+    setTasks([]);
+  }
+
+  return (
+    <div className="App">
+      <h1>To-Do App</h1>
+      <input
+        type="text"
+        placeholder="Enter a task"
+        value={task}
+        onChange={(e) => setTask(e.target.value)}
+      />
+
+      <button onClick={handleAdd}>Add Task</button>
+      <button id="del-button" onClick={handleDeleteAll}>Delete All</button>
+      
+      <ul>
+        {tasks.map((t, i) => (
+          <li key={i}>
+            {t} <button onClick={() => handleDelete(i)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+
+      
+    </div>
+  );
+}
+export default App;
